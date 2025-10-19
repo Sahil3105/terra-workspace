f284857de22e758f4162258e6965af51d1f020c3
terra <terrauser123@gmail.com>
2025-10-19 07:47:09 +0000

    Files are added to main branch

---
+++ ec2.tf
@@ -0,0 +1,64 @@
+resource "aws_key_pair" "demo" {
+  key_name   = "terra-key-ec2"
+  public_key = file("terra-key-ec2.pub")
+}
+
+resource "aws_default_vpc" "defvpc" {}
+
+resource "aws_security_group" "allow_user_to_connect" {
+  name        = "terra-sg"
+  description = "Allow user to connect"
+  vpc_id      = aws_default_vpc.defvpc.id
+
+  ingress {
+    description = "port 22 allow"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 80 allow"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 443 allow"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outgoing traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysecurity"
+  }
+}
+
+resource "aws_instance" "testinstance" {
+  ami                    = "ami-0360c520857e3138f"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.demo.key_name
+  vpc_security_group_ids = [aws_security_group.allow_user_to_connect.id]
+
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "Terra-Automate"
+  }
+}
